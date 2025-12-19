3af73d536bb2b61d28df0129479892432ee76c71
gustavoakihiro <akihiromatsuoka@hotmail.com>
2025-12-19 16:25:51 -0300

    Project created

---
+++ src/main/java/com/gustavoakihiro/studyproject/StudyprojectApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoakihiro.studyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavoakihiro/studyproject/StudyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoakihiro.studyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
