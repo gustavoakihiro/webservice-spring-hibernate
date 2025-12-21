a4f4244730cefcc6b23bb1fd19b79aca70274eb6
gustavoakihiro <akihiromatsuoka@hotmail.com>
2025-12-21 16:02:37 -0300

    H2 database test profile JPA

--- src/main/java/com/gustavoakihiro/studyproject/entities/User.java
+++ src/main/java/com/gustavoakihiro/studyproject/entities/User.java
@@ -2,20 +2,27 @@ package com.gustavoakihiro.studyproject.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb=user")
 public class User implements Serializable {
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
+
 	public User() {
-		
+
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -92,5 +99,4 @@ public class User implements Serializable {
 		return true;
 	}
 
-	
 }
