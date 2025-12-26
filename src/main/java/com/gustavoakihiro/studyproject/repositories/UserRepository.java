78ac2d04fae1976b2d93e67e75a5fc31e3c67424
gustavoakihiro <akihiromatsuoka@hotmail.com>
2025-12-26 19:58:50 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/gustavoakihiro/studyproject/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gustavoakihiro.studyproject.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gustavoakihiro.studyproject.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+
+}
