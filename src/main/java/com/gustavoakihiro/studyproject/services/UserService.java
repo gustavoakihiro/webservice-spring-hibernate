944a825c8c3c0b1344bdc23ebfde87f119554e26
gustavoakihiro <akihiromatsuoka@hotmail.com>
2025-12-29 21:55:51 -0300

    Service layer, component registration

---
+++ src/main/java/com/gustavoakihiro/studyproject/services/UserService.java
@@ -0,0 +1,27 @@
+package com.gustavoakihiro.studyproject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavoakihiro.studyproject.entities.User;
+import com.gustavoakihiro.studyproject.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
